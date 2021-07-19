8320c962de76f01d2297a6afd69839a3265a4ea3
Danilo Miranda <danilo030920@gmail.com>
2021-07-19 09:30:12 -0300

    card de produtos componentizado

---
+++ src/app/components/product-card/product-card.component.ts
@@ -0,0 +1,25 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { CartService } from 'src/app/services/cart.service';
+import { EffectsService } from 'src/app/services/effects.service';
+import { MocksService } from 'src/app/services/mocks.service';
+import { Product } from 'src/app/models/product';
+import { fadeInOut } from 'src/app/animations';
+
+@Component({
+  selector: 'app-product-card',
+  templateUrl: './product-card.component.html',
+  styleUrls: ['./product-card.component.scss'],
+  animations: [fadeInOut],
+})
+export class ProductCardComponent implements OnInit {
+  @Input() product: Product;
+
+  constructor(
+    public cartService: CartService,
+    public effectsService: EffectsService,
+    public mocksService: MocksService
+  ) {}
+
+  ngOnInit(): void {}
+}
