7a9ae2a64d83ac719e0fe90d5f7cad683cd1e241
Danilo Miranda <danilo030920@gmail.com>
2021-07-19 09:33:53 -0300

    card de produtos componentizado

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
 import { CartService } from 'src/app/services/cart.service';
+import { EffectsService } from 'src/app/services/effects.service';
 import { MocksService } from 'src/app/services/mocks.service';
-import { fadeInOut } from 'src/app/animations'
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
-  animations: [fadeInOut]
 })
 export class MainComponent implements OnInit {
   constructor(
     public cartService: CartService,
+    public effectsService: EffectsService,
     public mocksService: MocksService
   ) {}
 
